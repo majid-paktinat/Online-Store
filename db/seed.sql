1e0cec3a2eaf9c45284117cdf1729dcdee36a5a0
Sahar <sahar.sagharichi@gmail.com>
2020-03-10 12:20:01 -0400

    test

--- db/seed.sql
+++ db/seed.sql
@@ -4,10 +4,9 @@ INSERT INTO categories (categoryname) VALUES ('Sofas');
 INSERT INTO categories (categoryname) VALUES ('Chairs');
 INSERT INTO categories (categoryname) VALUES ('Tables');
 
-select * from categories;
-
 INSERT INTO products (categoryid,productname,productdescription,productimage) VALUES ('Sofas');
 
+INSERT INTO products (categoryid,productname,productdescription,productimage) VALUES ('chaires');
 
 
 
