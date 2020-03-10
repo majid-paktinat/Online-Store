ccee4e852e5afef091fae85e541c7328db908fa5
Sahar <sahar.sagharichi@gmail.com>
2020-03-10 12:17:27 -0400

    Categories table created

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,18 @@
 USE onlinestore_db;
 
+INSERT INTO categories (categoryname) VALUES ('Sofas');
+INSERT INTO categories (categoryname) VALUES ('Chairs');
+INSERT INTO categories (categoryname) VALUES ('Tables');
+
+select * from categories;
+
+INSERT INTO products (categoryid,productname,productdescription,productimage) VALUES ('Sofas');
+
+
+
+
+
+
+
 
 
