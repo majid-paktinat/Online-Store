9c0c9bbea32b064f35ae257a123f55f3e7ef195f
majid <majid.paktinat@gmail.com>
2020-03-15 19:30:26 -0400

    order checkout codes are updated

--- public/assets/clientjs/checkout.js
+++ public/assets/clientjs/checkout.js
@@ -12,25 +12,49 @@ $.get( `/api/cartFields` ).then(function(response){
     document.getElementById('hiduserid').value = response.USERID;
     document.getElementById('hidproductid').value = response.PRODUCTID;
     document.getElementById('hidproductquantity').value = response.PRODUCTQUANTITY;
-    console.log(response.USERID);  // agar direct az addressbar biyad too cart.html ina empty hastan... (bayad control konim!)
+    // console.log(response.USERID);  // agar direct az addressbar biyad too checkout.html ina empty hastan... (bayad control konim!)
+
+                //Send the GET request
+                $.get( `/api/cart/${document.getElementById('hiduserid').value}` ).then(function(response){
+                
+                    ulStr = ` <h2>Your Order</h2>
+                    <ul class="list">
+                        <li>
+                        <a href="#">Product
+                            <span>Total</span>
+                        </a>
+                        </li>`;
+
+                    let totalAmount = 0;
+                    for (i=0; i<response.length; i++){
+                        let totalRow=0;
+                        totalRow = number_format(Number(response[i].productquantity)*Number(response[i].productprice), 2);
+                        ulStr = ulStr + 
+                                    `<li>
+                                    <a href="#">${response[i].productname}
+                                        <span class="middle">x ${response[i].productquantity}</span>
+                                        <span class="last">$${totalRow}</span>
+                                    </a>
+                                    </li>`
+                        
+                        totalAmount = Number(totalAmount) + Number(totalRow);
+                    }
+
+                ulStr = ulStr + ` </ul>`; 
+                $("#myorderbox").prepend(ulStr);
+
+                totalAmount = number_format(totalAmount,2);
+                $("#totalAmount").html(`<span>$${totalAmount}</span>`);
+
+                totalAmount = Number(totalAmount) + Number(50);
+                $("#totalAmountIncludingShipping").html(`<span>$${number_format(totalAmount,2)}</span>`);
+                
+                
 
 
-//Send the GET request
-$.get( `/api/cart/${document.getElementById('hiduserid').value}` ).then(function(response){
-  
-  for (i=0; i<response.length; i++){
-    
-    console.log(response[i]);
-  
-  }
-
+                });
 
 });
-
-
-
-});
-
 
 
 
@@ -57,3 +81,12 @@ $.get( `/api/cart/${document.getElementById('hiduserid').value}` ).then(function
 //   );
 
 });
+
+
+function number_format(val, decimals){
+    //Parse the value as a float value
+    val = parseFloat(val);
+    //Format the value w/ the specified number
+    //of decimal places and return it.
+    return val.toFixed(decimals);
+}
